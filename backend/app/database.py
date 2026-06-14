fb8065b66ebf2a5cdd8568695da2f8fc7fa3beac
Kir Koov <k.koovshinov@gmail.com>
2026-06-14 10:10:15 +0300

    Change database.py to accommodate tests for api.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,16 @@
+import os
+
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-DATABASE_URL = "sqlite+aiosqlite:///./piko.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite+aiosqlite:///./piko.db",
+)
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
 
 SessionLocal = async_sessionmaker(
     bind=engine,
