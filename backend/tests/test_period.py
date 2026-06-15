0d891897dfe61eeda57c71a287436885f69388ac
Kir Koov <k.koovshinov@gmail.com>
2026-06-15 18:33:24 +0300

    Refactor test_period.py for less lines of code.

--- backend/tests/test_period.py
+++ backend/tests/test_period.py
@@ -2,24 +2,16 @@ from app.period import period_for_date
 
 
 def test_period_start():
-    start, end = period_for_date("2026-06-08")
-    assert start == "2026-06-08"
-    assert end == "2026-06-28"
+    assert period_for_date("2026-06-08") == ("2026-06-08", "2026-06-28")
 
 
 def test_period_middle():
-    start, end = period_for_date("2026-06-20")
-    assert start == "2026-06-08"
-    assert end == "2026-06-28"
+    assert period_for_date("2026-06-20") == ("2026-06-08", "2026-06-28")
 
 
 def test_next_period():
-    start, end = period_for_date("2026-06-29")
-    assert start == "2026-06-29"
-    assert end == "2026-07-19"
+    assert period_for_date("2026-06-29") == ("2026-06-29", "2026-07-19")
 
 
 def test_last_day_of_period():
-    start, end = period_for_date("2026-06-28")
-    assert start == "2026-06-08"
-    assert end == "2026-06-28"
+    assert period_for_date("2026-06-28") == ("2026-06-08", "2026-06-28")
