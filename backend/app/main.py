6badcc8616a747137fa060e4cc76a746287e52ec
Kir Koov <k.koovshinov@gmail.com>
2026-07-01 23:31:45 +0300

    Clean up main.py & get ready to rectify the rest of endpoints to their routers.

--- backend/app/main.py
+++ backend/app/main.py
@@ -104,43 +104,6 @@ def shift_to_dict(s: Shift) -> dict:
     }
 
 
-# @app.delete("/users/{user_id}")
-# async def delete_user(
-#     user_id: int,
-#     auth: tuple[User, Session] = Depends(get_current_user),
-#     db: AsyncSession = Depends(get_db),
-# ) -> dict:
-
-#     current_user, _ = auth
-#     if not current_user.is_admin:
-#         raise HTTPException(
-#             status_code=403,
-#             detail="Admin privileges required",
-#         )
-#     result = await db.execute(select(User).where(User.id == user_id))
-#     user = result.scalars().first()
-
-#     if user is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="User not found",
-#         )
-
-#     if user.id == current_user.id:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="Admin cannot delete themselves",
-#         )
-
-#     await db.delete(user)
-#     await db.commit()
-
-#     return {
-#         "status": "deleted",
-#         "id": user.id,
-#     }
-
-
 @app.post("/shifts")
 async def create_shift(
     date: str,
